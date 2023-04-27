f0adbb4f2923a8156b6db08f9761895c1ee4eb72
Chris Danielson <christopher.a.danielson@protonmail.com>
2023-04-26 19:32:37 -0700

    Add user prompt for squares per side

--- script.js
+++ script.js
@@ -9,15 +9,20 @@ rainbowModeButton.addEventListener("click", () => (rainbowMode = !rainbowMode));
 const clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", clearDoodles);
 
-function createGrid(container) {
-  for (let i = 0; i < 100; i++) {
+const GRID_WIDTH = 960;
+
+function createGrid(container, size) {
+  let cellDimensions = Math.floor(GRID_WIDTH / size);
+  for (let i = 0; i < size; i++) {
     const row = document.createElement('div');
     row.setAttribute('id', `row-${i}`);
     row.classList.add('row');
-    for (let j = 0; j < 100; j++) {
+    for (let j = 0; j < size; j++) {
       const cell = document.createElement('div');
       cell.setAttribute('id', `col-${j}`);
       cell.classList.add('cell');
+      cell.setAttribute('style', `width: ${cellDimensions}px; height: ${cellDimensions}px;`);
+      row.appendChild(cell);
       cell.addEventListener('mouseover', draw);
       cell.addEventListener('mousedown', draw);
       row.appendChild(cell);
@@ -29,9 +34,9 @@ function createGrid(container) {
 function draw(e) {
   if (e.type === 'mouseover' && mouseDown) {
     if (rainbowMode) { 
-      e.target.setAttribute('style', `background: ${randomColor()};`);
+      e.target.style['backgroundColor'] = randomColor();
     } else {
-      e.target.setAttribute('style', 'background: black;');
+      e.target.style['backgroundColor'] = 'black';
     }
   }
 }
@@ -51,8 +56,16 @@ function clearDoodles() {
   window.location.reload();
 }
 
+function promptUserForGridSize() {
+  let squaresPerSide = prompt("How many squares per side? ");
+  if (squaresPerSide != null) { return parseInt(squaresPerSide) };
+  return 100;
+}
+
 // main program
 const doodlePad = document.querySelector('.doodlepad')
 window.onload = function() {
-  createGrid(doodlePad);
+  let size = promptUserForGridSize();   
+  console.log(size);
+  createGrid(doodlePad, size);
 };
